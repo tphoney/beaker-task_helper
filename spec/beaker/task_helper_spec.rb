6852b308b9ffa8877181428759459b63313502dd
tphoney <tp@puppet.com>
2017-10-11 15:46:34 +0100

    further improvements

--- spec/beaker/task_helper_spec.rb
+++ spec/beaker/task_helper_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
+require 'spec_helper_acceptance'
 
-RSpec.describe Beaker::I18nHelper do # rubocop:disable Metrics/BlockLength
+RSpec.describe Beaker::I18nHelper do 
   it 'has a version number' do
     expect(Beaker::I18nHelper::VERSION).not_to be nil
   end
@@ -13,26 +13,6 @@ RSpec.describe Beaker::I18nHelper do # rubocop:disable Metrics/BlockLength
     context 'with valid args' do
       ['ja_JP', 'ja-JP', 'ja_JP.utf-8', 'ja-JP.UTF-8'].each do |lang|
         it_behaves_like 'a valid lang string', lang
-      end
-    end
-  end
-  describe '#parse_lang' do
-    context 'underscored locale string' do
-      let(:val) { parse_lang('ja_JP.utf8') }
-      it 'returns an array' do
-        expect(val).to be_kind_of(Array)
-      end
-      it 'returns correct information' do
-        expect(val).to eq(%w[ja JP])
-      end
-    end
-    context 'hyphenated locale string' do
-      let(:val) { parse_lang('ja-JP.utf8') }
-      it 'returns an array' do
-        expect(val).to be_kind_of(Array)
-      end
-      it 'returns correct information' do
-        expect(val).to eq(%w[ja JP])
       end
     end
   end
